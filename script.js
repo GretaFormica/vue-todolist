d80d6f329a1e5de0b5ada2b9a8d3131fb03e9c55
GretaFormica <greta.formica@gmail.com>
2023-02-13 15:29:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//milestone 1
+const app = Vue.createApp({
+
+    data() { //metodo data
+
+        return {
+            tasks: [
+            ],
+
+            newTask: ""
+        }
+    },
+
+    methods: {
+
+    }
+});
+
+app.mount("#root");
